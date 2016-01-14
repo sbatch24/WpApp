9ec30cf56f9c161becb43212827116b3c54243df
Sekou Batchelor <sbatch24@gmail.com>
2016-01-14 15:29:48 -0500

    added wp-admin link. updated readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Test WP App
 
+## Getting Started
+'''bash
+bower install
+cordova platform add android
+npm install
+ionic state restore
+'''
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@ angular.module('wpIonic.controllers', [])
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $sce, DataLoader, $rootScope, $log ) {
   
   // Enter your site url here. You must have the WP-API v2 installed on this site. Leave /wp-json/wp/v2/ at the end.
-  $rootScope.url = 'http://www.mysite.com/wp-json/wp/v2/';
+  $rootScope.url = 'http://newdrakemusic.com/wp-json/wp/v2/';
 
   // $rootScope.callback = '_jsonp=JSON_CALLBACK';
 
